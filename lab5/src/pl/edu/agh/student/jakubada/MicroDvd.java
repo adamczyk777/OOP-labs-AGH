07b0718545c4e314b8a1c61c23e1e5ccd2e34685
Jakub Adamczyk <adamczyk777@gmail.com>
2017-10-26 22:53:14 +0200

    Idea files

--- lab5/src/pl/edu/agh/student/jakubada/MicroDvd.java
+++ lab5/src/pl/edu/agh/student/jakubada/MicroDvd.java
@@ -2,10 +2,15 @@ package pl.edu.agh.student.jakubada;
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
+/**
+ * MicroDvd class is a class that handles operations
+ * on microdvd subtitle format method that it uses
+ * are specified in Subtitle format interface
+ */
 public class MicroDvd implements SubtitlesFormat {
     @Override
     public void delay(String in, String out, int delay, int fps) {
-        
+
     }
 }
 /*--------------------------------------------------------------------------------------------------------------------*/
