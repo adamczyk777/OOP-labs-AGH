d061be42dbbce1523db65650dca408223964532f
Jakub Adamczyk <adamczyk777@gmail.com>
2017-10-26 22:41:08 +0200

    Add interface
    interface standarizes subtitle handling classes methods

---
+++ lab5/src/pl/edu/agh/student/jakubada/SubtitlesFormat.java
@@ -0,0 +1,20 @@
+package pl.edu.agh.student.jakubada;
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+/**
+ * SubtitlesFormat interface
+ * used for standarization of diffrent subtitle formats
+ * ex. MicroDvd is one format and SubRIP is another
+ * standarization is about single access methods interface to use them anywhere
+ */
+public interface SubtitlesFormat {
+    /**
+     * delay method:
+     * in: input file path
+     * out: output file path
+     * delay: delay of subtitles in milliseconds
+     * fps: move framerate in frames per second
+     */
+    void delay(String in, String out, int delay, int fps);
+}
+/*--------------------------------------------------------------------------------------------------------------------*/
