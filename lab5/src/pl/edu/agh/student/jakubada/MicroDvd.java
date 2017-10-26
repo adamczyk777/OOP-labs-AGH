440377564aaffad286a8fde80a2817fc243bac7c
Jakub Adamczyk <adamczyk777@gmail.com>
2017-10-26 22:41:40 +0200

    add class microdvd
    Class microdvd implemets SubtitleFormat and is used for a subtitles format as the name says

---
+++ lab5/src/pl/edu/agh/student/jakubada/MicroDvd.java
@@ -0,0 +1,11 @@
+package pl.edu.agh.student.jakubada;
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+public class MicroDvd implements SubtitlesFormat {
+    @Override
+    public void delay(String in, String out, int delay, int fps) {
+        
+    }
+}
+/*--------------------------------------------------------------------------------------------------------------------*/
