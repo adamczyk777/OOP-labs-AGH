ffb030de9fce9f637f5a198b7e2b5065c3961e77
Jakub Adamczyk <adamczyk777@gmail.com>
2017-11-03 00:32:51 +0100

    First implementation of indico sorter

---
+++ lab5new/src/pl/edu/agh/student/jakubada/ImageClassificator.java
@@ -0,0 +1,48 @@
+package pl.edu.agh.student.jakubada;
+
+import io.indico.Indico;
+import io.indico.api.results.BatchIndicoResult;
+import io.indico.api.utils.IndicoException;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ImageClassificator {
+    private File folder;
+    private Indico apiInstance;
+
+    public ImageClassificator(String folderPath, String apiKey) throws IndicoException {
+        this.folder = new File(folderPath);
+        this.apiInstance = new Indico(apiKey);
+    }
+
+    public Map<String, String> getMostMatchingFeatureValues() throws IndicoException, IOException {
+        // Geting File paths
+        File[] photosFileList = folder.listFiles();
+        String[] photoPathList = new String[photosFileList.length];
+        for (int i = 0; i < photoPathList.length; i++) {
+            photoPathList[i] = photosFileList[i].getAbsolutePath();
+        }
+        // Api Call
+        BatchIndicoResult multiple = this.apiInstance.imageRecognition.predict(photoPathList);
+        List<Map<String, Double>> imageRecognition = multiple.getImageRecognition();
+
+        Map<String, String> results = new HashMap<>();
+        int i = 0;
+        for (Map<String, Double> map : imageRecognition) {
+            Map.Entry<String, Double> maxEntry = null;
+            for (Map.Entry<String, Double> entry : map.entrySet()) {
+                if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0) {
+                    maxEntry = entry;
+                }
+            }
+            results.put(photoPathList[i], maxEntry.getKey());
+            i++;
+        }
+        return results;
+    }
+}
--- lab5new/src/pl/edu/agh/student/jakubada/Main.java
+++ lab5new/src/pl/edu/agh/student/jakubada/Main.java
@@ -1,8 +1,36 @@
 package pl.edu.agh.student.jakubada;
 
+import io.indico.api.utils.IndicoException;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Map;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
+        String PATH = "C:\\Users\\jadamczyk\\Documents\\GitRepositories\\programowanie-obiektowe-agh\\lab5new\\";
+        try {
+            ImageClassificator classificator = new ImageClassificator(
+                    PATH + "foto",
+                    "5357d5b1a333fd6a0cd4a19647f9f338");
+            for (Map.Entry<String, String> entry : classificator.getMostMatchingFeatureValues().entrySet()) {
+                File newDir = new File(PATH + "output\\" + entry.getValue());
+                if (!newDir.exists()) {
+                    newDir.mkdir();
+                }
+                File image = new File(entry.getKey());
+                Files.copy(image.toPath(), new File(newDir.getAbsolutePath() + "\\" + image.getName()).toPath(), REPLACE_EXISTING);
+            }
+        } catch (IndicoException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
